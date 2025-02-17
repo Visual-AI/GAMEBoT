8107587379188af932fe1914faf1ff5f9cec9e1a
linwenye <419766878@qq.com>
2025-02-17 22:04:08 +0800

    delete no use file

--- debug.sh
+++
@@ -1,9 +0,0 @@
-# python run_games_and_check/texas_no_limit_v6.py gpt-4o random --cycles 1
-# python run_games_and_check/texas_no_limit_v6.py random random --cycles 1
-
-# python run_games_and_check/connect4.py random random --cycles 1
-# python run_games_and_check/tictactoe.py gpt-4o random --cycles 1
-# python run_games_and_check/surround.py gpt-4o random --cycles 1
-# python run_games_and_check/multi-agent-asyn-atari_pong.py gpt-4o random --cycles 1
-# python run_games_and_check/othello.py gpt-4o random --cycles 1
-python run_games_and_check/checkers.py random random --cycles 1
